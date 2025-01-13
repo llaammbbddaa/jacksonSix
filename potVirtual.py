d9f0e20764b825274a759c6efb47294bfeecbb20
fishmem <108447025+fishmem@users.noreply.github.com>
2025-01-13 14:28:36 -0800

    Update potVirtual.py

--- potVirtual.py
+++ potVirtual.py
@@ -22,18 +22,21 @@ file.write("\n")
 
 counter = 0
 
+# Infinite loop to set wiper values and measure voltage
 while True:
     ds3502.wiper = 127
     print("Wiper set to %d" % ds3502.wiper)
+    # Read and calculate the voltage at the wiper output
     voltage = wiper_output.value
     voltage *= 3.3
-    voltage /= 65535
+    voltage /= 65535 # Normalize the value to get actual voltage
     print("Wiper voltage: %.2f V" % voltage)
     print("")
     file.write("wiper voltage " + str(voltage) + " V,")
     
     sleep(1.0)
-
+    
+    # Set the wiper to the lowest value (0) and measure voltage
     ds3502.wiper = 0	
     print("Wiper set to %d" % ds3502.wiper)
     voltage = wiper_output.value
@@ -44,6 +47,7 @@ while True:
     print("")
     sleep(1.0)
 
+    # Set the wiper to an intermediate value (63) and measure voltage
     ds3502.wiper = 63
     print("Wiper set to %d" % ds3502.wiper)
     voltage = wiper_output.value
@@ -53,9 +57,11 @@ while True:
     file.write("wiper voltage " + str(voltage) + " V,")
     print("")
     sleep(1.0)
-    
+
+    # Break the loop after 3 iterations
     counter = counter + 1
     if counter >= 3:
         break
-
+        
+# Close the file to save the logged data
 file.close()
