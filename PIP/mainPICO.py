c10839bfb7907bf4aa68a0f6bfb3d4005b6542cb
fishmem <108447025+fishmem@users.noreply.github.com>
2025-01-13 14:59:39 -0800

    Update mainPICO.py

--- PIP/mainPICO.py
+++ PIP/mainPICO.py
@@ -9,12 +9,15 @@ import time
 # all of the sensors in a list
 # in the order of [tempSensor, voltSensor, dht]
 allSensors = dataOutput_PIP.beginSensors()
-tempSensor = allSensors[0]
-voltSensor = allSensors[1]
-dht = allSensors[2]
+tempSensor = allSensors[0]# MAX31865 temperature sensor
+voltSensor = allSensors[1]# INA219 voltage/current sensor
+dht = allSensors[2] # DHT11 temperature/humidity sensor
 
+# Infinite loop for continuous operation
 while True:
+    # Listen for commands sent to the Pico (e.g., over serial)
     pS_PICO_PIP.beginListen()
+    # Collect and print data from sensors
     dataOutput_PIP.beginSensing(tempSensor, voltSensor, dht)
     time.sleep(1)
 
